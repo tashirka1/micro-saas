d18eb1bb76c0179a92df7b6072338b84002e7fab
Rishat Sharafiev <tashirka1@gmail.com>
2026-05-18 10:41:15 +0300

    fix

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -4,9 +4,5 @@
 </script>
 
 <Header />
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<h1>Hello world</h1>
 <Footer />
